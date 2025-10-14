762ab5857d2f6dfc00dcfb9698b0436f068c91a5
EvanA4 <evanabbott04@gmail.com>
2025-10-14 12:41:04 -0400

    basic formatting and props for map

--- components/MapDemo.tsx
+++ components/MapDemo.tsx
@@ -3,16 +3,18 @@
 import dynamic from 'next/dynamic';
 import { useMemo } from 'react';
 
-export default function MyPage() {
-  const Map = useMemo(() => dynamic(
-    () => import('@/components/MapWrapper'),
-    { 
-      loading: () => <p>A map is loading</p>,
-      ssr: false
-    }
-  ), [])
+function MapDemo() {
+	const MapWrapper = useMemo(() => dynamic(
+		() => import('@/components/MapWrapper'),
+		{ 
+			loading: () => <p>A map is loading</p>,
+			ssr: false
+		}
+	), []);
 
-  return <div className='w-[50%] h-[50%]'>
-    <Map />
-  </div>
+	return (
+		<MapWrapper position={[35.155556, -90.051944]} zoom={15} className='w-[50%] h-[50%]'/>
+	);
 }
+
+export default MapDemo;
--- components/MapWrapper.tsx
+++ components/MapWrapper.tsx
@@ -1,23 +1,32 @@
-// src/components/Map.tsx
+'use client';
+
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 import { LatLngTuple } from "leaflet";
 
-export default function MyMap(props: any) {
-  // const { position, zoom } = props
-  const position: LatLngTuple = [35.155556, -90.051944];
+function MapWrapper(props: {
+	position: number[],
+	zoom: number,
+	className?: string
+}) {
+	const position = props.position as LatLngTuple;
+	const zoom = props.zoom;
 
-  return <MapContainer center={position} zoom={15} scrollWheelZoom={true} className="h-full w-full">
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+	return (
+		<MapContainer center={position} zoom={15} scrollWheelZoom={true} className={props.className}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}>
+				<Popup>
+				The divine Bass Pro Shop pyramid. <br /> A modern world wonder.
+				</Popup>
+			</Marker>
+		</MapContainer>
+	);
 }
+
+export default MapWrapper;
