14969c7c56bf6fb18b603bc21ecddd57a52ab509
EvanA4 <evanabbott04@gmail.com>
2026-01-25 10:04:00 -0500

    second test after rename

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Profile() {
   return (
     <div className="h-full relative p-10">
-      Epic profile page test
+      Epic profile page
       {/* Navbar */}
       <Nav />
     </div>
