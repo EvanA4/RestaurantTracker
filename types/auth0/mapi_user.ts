d78bf3c38c575aa10ac31e5fcb67787f0f904873
EvanA4 <evanabbott04@gmail.com>
2026-01-26 13:48:04 -0500

    changed order of fields in type MAPIUser

--- types/auth0/mapi_user.ts
+++ types/auth0/mapi_user.ts
@@ -1,13 +1,13 @@
 export type MAPIUser = {
+  user_id: string;
+  name: string;
+  nickname: string;
+  given_name: string;
+  family_name: string;
+  email: string;
+  picture: string;
   email_verified: boolean;
   idp_tenant_domain?: string;
-  email: string;
-  name: string;
-  picture: string;
-  updated_at: string;
-  user_id: string;
-  family_name: string;
-  given_name: string;
   identities: {
     access_token: string;
     expires_in: number;
@@ -16,9 +16,9 @@ export type MAPIUser = {
     provider: string;
     isSocial: boolean;
   }[];
-  nickname: string;
-  created_at: string;
   last_login: string;
   last_ip: string;
   logins_count: number;
+  created_at: string;
+  updated_at: string;
 };
