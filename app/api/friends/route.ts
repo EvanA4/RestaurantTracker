fafdfbe8f5c9fd32e95860200c155edaef2e888c
amy-huang16 <ahuang16@vols.utk.edu>
2026-02-12 13:19:40 -0500

    add get put post

--- app/api/friends/route.ts
+++ app/api/friends/route.ts
@@ -1,17 +1,53 @@
-// GET
-// DELETE
-
 import FriendModel from "@/models/Friend";
 import dbConnect from "@/utils/dbconnect";
 import { NextRequest, NextResponse } from "next/server";
 
-export const deleteFriend = async function (
+export const GET = async function (
+  req: NextRequest,
+  { params }: { params: { userId: string } },
+) {
+  await dbConnect();
+  const friend = await FriendModel.find({
+    $or: [{ requestorId: params.userId }, { receiverId: params.userId }],
+  });
+  return NextResponse.json(friend, { status: 200 });
+};
+
+// the same as creating a new Friend relationship / sending a friend request
+export const POST = async function (
+  req: NextRequest,
+  { params }: { params: { requestorId: string; receiverId: string } },
+) {
+  await dbConnect();
+
+  const friend = await FriendModel.insertOne({
+    requestorId: params.requestorId,
+    receiverId: params.receiverId,
+    status: false,
+  });
+  return NextResponse.json(friend, { status: 200 });
+};
+
+// accept an existing friend request
+export const PUT = async function (
+  req: NextRequest,
+  { params }: { params: { requestorId: string; receiverId: string } },
+) {
+  await dbConnect();
+  const friend = await FriendModel.findOneAndUpdate(
+    { requestorId: params.requestorId, receiverId: params.receiverId },
+    { status: true },
+    { new: true },
+  );
+  return NextResponse.json(friend, { status: 200 });
+};
+
+// unsending a friend request or denying a friend request
+export const DELETE = async function (
   req: NextRequest,
   { params }: { params: { id: string } },
 ) {
   await dbConnect();
-
-  // add some error checking here with other status codes
 
   const friend = await FriendModel.findOneAndDelete({ _id: params.id });
   return NextResponse.json(friend, { status: 200 });
