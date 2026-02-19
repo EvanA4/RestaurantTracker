9de6137d320c329514d69b0c780be19e5b805c03
amy-huang16 <ahuang16@vols.utk.edu>
2026-02-19 17:16:05 -0500

    renamed for clarity

--- app/api/friends/manage/[friendRequestId]/route.ts
+++ app/api/friends/manage/[friendRequestId]/route.ts
@@ -5,18 +5,18 @@ import { NextRequest, NextResponse } from "next/server";
 // accept an existing friend request
 export const PUT = async function (
   req: NextRequest,
-  { params }: { params: { friendId: string } },
+  { params }: { params: { friendRequestId: string } },
 ) {
   await dbConnect();
 
-  if (!params.friendId)
+  if (!params.friendRequestId)
     return NextResponse.json(
       { message: "Friend ID required" },
       { status: 400 },
     );
 
   const friend = await FriendModel.findOneAndUpdate(
-    { _id: params.friendId },
+    { _id: params.friendRequestId },
     { status: true },
     { new: true },
   );
@@ -30,17 +30,19 @@ export const PUT = async function (
 // unsending a friend request or denying a friend request
 export const DELETE = async function (
   req: NextRequest,
-  { params }: { params: { friendId: string } },
+  { params }: { params: { friendRequestId: string } },
 ) {
   await dbConnect();
 
-  if (!params.friendId)
+  if (!params.friendRequestId)
     return NextResponse.json(
       { message: "Friend ID required" },
       { status: 400 },
     );
 
-  const friend = await FriendModel.findOneAndDelete({ _id: params.friendId });
+  const friend = await FriendModel.findOneAndDelete({
+    _id: params.friendRequestId,
+  });
 
   if (!friend)
     return NextResponse.json({ message: "Friend not found" }, { status: 404 });
