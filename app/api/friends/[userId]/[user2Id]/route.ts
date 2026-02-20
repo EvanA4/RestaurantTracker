4fe3ad514c70094ff799ddb64bb6bd511f81bf8b
amy-huang16 <ahuang16@vols.utk.edu>
2026-02-19 19:05:25 -0500

    one more error check

--- app/api/friends/[userId]/[user2Id]/route.ts
+++ app/api/friends/[userId]/[user2Id]/route.ts
@@ -1,4 +1,5 @@
 import FriendModel from "@/models/Friend";
+// import UserModel from "@/models/User";
 import dbConnect from "@/utils/dbconnect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -36,15 +37,39 @@ export const POST = async function (
 ) {
   await dbConnect();
 
+  // verify that both userId and user2Id are present
   if (!params.userId || !params.user2Id)
     return NextResponse.json(
       { message: "Requestor User ID and Receiver User ID required" },
       { status: 400 },
     );
 
+  // verify that the requestor and receiver user IDs are not the same
   if (params.userId == params.user2Id)
     return NextResponse.json(
       { message: "Requestor and Receiver User IDs cannot be the same" },
+      { status: 400 },
+    );
+
+  // verify that the users exist in the database
+  // const user = await UserModel.findOne({ _id: params.userId });
+  // if (!user)
+  //   return NextResponse.json({ message: "User not found" }, { status: 404 });
+
+  // const user2 = await UserModel.findOne({ _id: params.user2Id });
+  // if (!user2)
+  //   return NextResponse.json({ message: "User not found" }, { status: 404 });
+
+  // verify that the friend relationship does not already exist
+  const existing = await FriendModel.findOne({
+    $or: [
+      { requestorId: params.userId, receiverId: params.user2Id },
+      { requestorId: params.user2Id, receiverId: params.userId },
+    ],
+  });
+  if (existing)
+    return NextResponse.json(
+      { message: "Friend relationship already exists" },
       { status: 400 },
     );
 
