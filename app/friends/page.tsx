fb6c12a1caf677cd0fb6c4e61655c87faf7f8f16
Deep0320 <dpatel03204@gmail.com>
2026-02-19 22:56:07 -0500

    set up the layout of the friends page

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -2,14 +2,42 @@
 
 import Nav from "@/components/general/Nav";
 import React from "react";
+import FriendCard from "@/components/profile/friends/FriendCard";
 
 function Profile() {
+  const friends = [];
+  for (let i = 0; i < 11; ++i) {
+    friends.push(<FriendCard />);
+  }
   return (
     <div className="h-full relative">
-      <div className="h-full flex items-center justify-center">
-        <p>Friends page</p>
+      <div>
+        <p className="pt-[75px] pl-[10px] text-2xl border-b-1 border-b-gray-300">
+          Friends
+        </p>
       </div>
+      <div className="pt-[3%] flex flex-col md:flex-row items-center justify-around">
+        <div className="w-full flex flex-col overflow-hidden p-6 lg:p-10">
+          <div className="border-1 border-b-0 border-gray-300">
+            <p className="p-1 text-lg">Friends</p>
+          </div>
+          <div className="overflow-y-scroll !h-100 scrollbar-none divide-solid divide-y divide-gray-300 border-1 border-gray-300">
+            {...friends}
+          </div>
+        </div>
 
+        <div className="w-full flex flex-col overflow-hidden p-6 lg:p-10">
+          <div className="border-1 border-b-0 border-gray-300">
+            <p className="p-1 text-lg">Invites</p>
+          </div>
+          <div className="overflow-y-scroll !h-100 scrollbar-none divide-solid divide-y divide-gray-300 border-1 border-gray-300">
+            <p className="p-1 text-sm">Sent Invites</p>
+            {...friends}
+            <p className="p-1 text-sm">Pending Invites</p>
+            {...friends}
+          </div>
+        </div>
+      </div>
       {/* Bottom Navbar */}
       <Nav />
     </div>
---
+++ components/profile/friends/FriendCard.tsx
@@ -0,0 +1,3 @@
+export default function FriendCard() {
+  return <div className="!h-20 flex flex-row justify-between "></div>;
+}
